6a18f9ef37ec763c7a8e8775005dce69a9feb440
Muhammad Pidha Iqbal Fadillah <126032210+Qollie@users.noreply.github.com>
2023-09-24 09:31:32 +0800

    Update Segitiga Pythagoras.py

--- Segitiga Pythagoras.py
+++ Segitiga Pythagoras.py
@@ -15,31 +15,54 @@ print(f"Nama    : {nama}")
 print(f"NIM     : {nim}")
 print(f"Kelas   : SI {kelas} 2023")
 
-#perhitungan rumus pythagoras
+#perhitungan rumus segitiga pythagoras
 
 while True :
     hitung_sisi = input("Masukan Sisi yang mau dihitung (alas, tegak, miring) :")
 
-    if hitung_sisi == "alas" :
-        sisi_tegak = float(input("Masukan Sisi tegak : "))
-        sisi_miring = float(input("Masukan Sisi miring : "))
-        sisi_alas = (sisi_miring ** 2 - sisi_tegak ** 2) ** 0.5
-        print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi alasnya {sisi_alas}")
+    if hitung_sisi == "alas":
+        while True:
+            sisi_tegak = input("Masukan Sisi tegak: ")
+            sisi_miring = input("Masukan Sisi miring: ")
+            if sisi_tegak.isnumeric() and sisi_miring.isnumeric():
+                sisi_tegak = float(sisi_tegak)
+                sisi_miring = float(sisi_miring)
+                sisi_alas = (sisi_miring ** 2 - sisi_tegak ** 2) ** 0.5
+                print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi alasnya {sisi_alas}")
+                print(type(sisi_alas))
+                break
+            else:
+                print("Panjang sisi harus berupa angka. Silakan coba lagi.")
         break
 
     elif hitung_sisi == "tegak" :
-        sisi_alas = float(input("Masukan Sisi alas : "))
-        sisi_miring = float(input("Masukan Sisi miring : "))
-        sisi_tegak = (sisi_miring ** 2 - sisi_alas ** 2) ** 0.5
-        print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi tegaknya {sisi_tegak}")
+        while True:
+            sisi_alas = (input("Masukan Sisi alas : "))
+            sisi_miring = (input("Masukan Sisi miring : "))
+            if sisi_alas.isnumeric() and sisi_miring.isnumeric():
+                sisi_alas = float(sisi_alas)
+                sisi_miring = float(sisi_miring)
+                sisi_tegak = (sisi_miring ** 2 - sisi_alas ** 2) ** 0.5
+                print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi tegaknya {sisi_tegak}")
+                break
+            else:
+                print("Panjang sisi harus berupa angka. Silakan coba lagi.")
         break
 
     elif hitung_sisi == "miring" :
-        sisi_alas = float(input("Masukan Sisi alas : "))
-        sisi_tegak = float(input("Masukan Sisi tegak : "))
-        sisi_miring = (sisi_alas ** 2 + sisi_tegak ** 2) ** 0.5
-        print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi miringnya {sisi_miring}")
+        while True:
+            sisi_alas = (input("Masukan Sisi alas : "))
+            sisi_tegak = (input("Masukan Sisi tegak : "))
+            if sisi_alas.isnumeric() and sisi_tegak.isnumeric():
+                sisi_alas = float(sisi_alas)
+                sisi_tegak = float(sisi_tegak)
+                sisi_miring = (sisi_alas ** 2 + sisi_tegak ** 2) ** 0.5
+                print(f"Hasil dari perhitungan tersebut menunjukkan bahwa sisi miringnya {sisi_miring}")
+                break
+            else:
+                print("Panjang sisi harus berupa angka. Silakan coba lagi.")
         break
-
+    
     else : 
         print("Masukan pilihan sisi yang benar (alas, tegak, miring). Silahkan coba masukan lagi")
+
